f7f9d9fc99ea853780c59b876fb4789bf5168b0a
jiankaidang <jiankai.dang@nyu.edu>
2013-12-10 22:58:50 -0500

    elastic-mapreduce

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
+myflow_id=$1
+input_s3=$2
+output_directory=$3
+
+#Use AWS CLI to copy jar files to input_s3 (S3)
+aws s3 cp hadoop-relative-frequencies.jar $input_s3/
+aws s3 cp wikitext_500.txt $input_s3/
+
 startStripes=$(date +%s)
-hadoop jar hadoop-relative-frequencies.jar Stripes $1 $2 $3
+elastic-mapreduce -j $myflow_id --jar $input_s3/hadoop-relative-frequencies.jar --arg Stripes --arg s3://jiankaidang --arg wikitext_500.txt --arg $output_directory
+elastic-mapreduce -j $myflow_id --wait-for-steps
 timeStripes=$(expr $(date +%s) - $startStripes)
 
-cat $3/rfstripes/part-r-00000 > $3/rfstripes.txt
-rm -rf $3/rfstripes
+aws s3 cp $output_directory/rfstripes/part-r-00000 $output_directory/rfstripes.txt
 
 startPairs=$(date +%s)
-#hadoop jar hadoop-relative-frequencies.jar Pairs $1 $2 $3
+elastic-mapreduce -j $myflow_id --jar $input_s3/hadoop-relative-frequencies.jar --arg Pairs --arg s3://cs9223 --arg wikitext_500000.txt --arg $output_directory
+elastic-mapreduce -j $myflow_id --wait-for-steps
 timePairs=$(expr $(date +%s) - $startPairs)
 
-#cat $3/rfpairs/part-r-00000 > $3/rfpairs.txt
-#rm -rf $3/rfpairs
+aws s3 cp $output_directory/rfpairs/part-r-00000 $output_directory/rfpairs.txt
 
-echo $(expr $timePairs / $timeStripes) > $3/rfcomp.txt
+echo $(expr $timePairs / $timeStripes) > rfcomp-tmp.txt
+aws s3 cp rfcomp-tmp.txt $output_directory/rfcomp.txt
+rm rfcomp-tmp.txt
